f7c8f0b856d774413d12ccb4cb3755330fe55259
Rodrigo Kovalski <rodrigokvk@gmail.com>
2020-09-18 19:00:47 -0300

    Adicionando menu mobile e ajustando responsividade da seção de serviços

--- js/global.js
+++ js/global.js
@@ -9,3 +9,17 @@ function stickyNavBar() {
 		navbar.classList.remove("sticky");
 	}
 }
+
+function openMenu(){
+	var navbar = document.querySelector("#navbar");
+	navbar.style.display = "block";
+	var closeButton = document.querySelector(".close-toggle");
+	closeButton.style.display = "block";
+}
+
+function closeMenu(){
+	var navbar = document.querySelector("#navbar");
+	navbar.style.display = "none";
+	var closeButton = document.querySelector(".close-toggle");
+	closeButton.style.display = "none";
+}
